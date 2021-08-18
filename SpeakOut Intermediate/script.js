33d45b0f28451cdcc1a1fffbfb89d64fb4ca8dcc
Daria Kozina <kozina.flowers@yandex.ru>
2021-08-18 12:25:02 +0300

    speakout fixed header in mobile

--- SpeakOut Intermediate/script.js
+++ SpeakOut Intermediate/script.js
@@ -168,3 +168,16 @@ const showLessonWords = () => {
 };
 showLessonWords();
 
+const fixHeader = () => {
+    const header = document.querySelector('.header');
+    window.addEventListener('scroll', () => {
+        if (pageYOffset > 0) {
+            header.classList.add('fixed');
+        } else {
+            header.classList.remove('fixed');
+        }
+    });
+};
+
+fixHeader();
+
