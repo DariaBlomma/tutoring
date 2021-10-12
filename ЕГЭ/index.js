9f72d2a4cee8c8930d648e611a924cab38e85568
Daria Kozina <kozina.flowers@yandex.ru>
2021-10-12 23:08:39 +0300

    ЕГЭ: corrected lc saving before blur

--- ЕГЭ/index.js
+++ ЕГЭ/index.js
@@ -11,22 +11,26 @@ const markAnswered = (item) => {
     }
 };
 
+const saveProgress = (elems) => {
+    const n10_9983 = [];
+    elems.forEach(item => {
+        n10_9983.push(item.value);
+    });
+    localStorage.setItem('n10_9983', JSON.stringify(n10_9983));
+};
+
 // for number 10, id 8893
 const inputs = document.querySelectorAll('.input.ex-10');
+if (!localStorage.getItem('n10_9983')) {
+    saveProgress(inputs);
+}
 inputs.forEach((item, index) => {
     item.value = JSON.parse(localStorage.getItem('n10_9983'))[index] || '';
     markAnswered(item);
 });
 
-
 document.addEventListener('blur', event => {
     const target = event.target;
-
     markAnswered(target);
-
-    const n10_9983 = [];
-    inputs.forEach(item => {
-        n10_9983.push(item.value);
-    });
-    localStorage.setItem('n10_9983', JSON.stringify(n10_9983));
+    saveProgress(inputs);
 }, true)
